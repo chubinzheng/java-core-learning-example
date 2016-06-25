d5117f15b2aa610178edf5195b12192b6a570cc7
zhengchubin <zhengchubin@192.168.1.114>
2016-06-25 16:36:06 +0800

    测试

---
+++ src/main/java/org/javacore/collection/map/TreeMapT.java
@@ -0,0 +1,41 @@
+package org.javacore.collection.map;
+
+import java.util.Map;
+import java.util.TreeMap;
+/*
+ * Copyright [2015] [Jeff Lee]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *  * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Jeff Lee
+ * @since 2015-6-18 21:36:04
+ * 	TreeMap 的使用
+ * TreeMap如不指定排序器，默认将按照key值进行升序排序，如果指定了排序器，则按照指定的排序器进行排序。
+ */
+public class TreeMapT
+{	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args)
+	{
+		Map map = new TreeMap();
+		map.put("1", "1");
+		map.put("5", "3");
+		map.put("a", "1");
+		map.put("4", "4");
+		map.put("2", "2");
+		map.put("2", "3");
+		// 注意排序
+		System.out.println(map);
+	}
+}
